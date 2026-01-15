f9310ccee3154b9467cf9d702b502929fe77a404
William Pieh <wtpieh@gmail.com>
2026-01-14 22:22:07 -0700

    Initial Commit

---
+++ TestUI/TestUIApp.swift
@@ -0,0 +1,32 @@
+//
+//  TestUIApp.swift
+//  TestUI
+//
+//  Created by William Pieh on 1/14/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct TestUIApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ TestUITests/TestUITests.swift
@@ -0,0 +1,17 @@
+//
+//  TestUITests.swift
+//  TestUITests
+//
+//  Created by William Pieh on 1/14/26.
+//
+
+import Testing
+@testable import TestUI
+
+struct TestUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
